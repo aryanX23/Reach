aa1bdf95d5cd2fbd1d9f99bd2d8f72344e53ec3a
Aryan Rai <aryanrai2304@gmail.com>
2024-06-29 15:26:02 +0530

    feat: initial setup for reach monorepo structure

---
+++ apps/reach-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-400 border-spacing-1'>
+        Hello this is working
+      </div>
+    </>
+  )
+}
+
+export default App
